5d719652086613856450fe4ac31553561e43bbfc
Ibtissam ELGHOURRASSE <100447916+Elghourrasse1999@users.noreply.github.com>
2023-01-15 18:10:03 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,507 @@
+
+function letterGuess1() {
+    var letter01 = letter1.value;
+    if (letter01 != "x") {
+      x1 = document.getElementById("letter1");
+      x1.classList.add("incorrect");
+      }
+    if (letter01 == "x") {
+      y1 = document.getElementById("letter1");
+      y1.classList.remove("incorrect");
+      y1.classList.add("correct");
+    }
+  }
+  function letterGuess2() {
+    var letter02 = letter2.value;
+    if (letter02 != "i") {
+      x2 = document.getElementById("letter2");
+      x2.classList.add("incorrect");
+      }
+    if (letter02 == "i") {
+    y2 = document.getElementById("letter2");
+    y2.classList.remove("incorrect");
+    y2.classList.add("correct");
+    }
+}
+
+function letterGuess3() {
+  var letter03 = letter3.value;
+  if (letter03 != "m") {
+    x3 = document.getElementById("letter3");
+    x3.classList.add("incorrect");
+    }
+  if (letter03 == "m") {
+  y3 = document.getElementById("letter3");
+  y3.classList.remove("incorrect");
+  y3.classList.add("correct");
+  }
+}
+
+function letterGuess4() {
+  var letter04 = letter4.value;
+  if (letter04 != "a") {
+    x4 = document.getElementById("letter4");
+    x4.classList.add("incorrect");
+    }
+  if (letter04 == "a") {
+  y4 = document.getElementById("letter4");
+  y4.classList.remove("incorrect");
+  y4.classList.add("correct");
+  }
+}
+
+function letterGuess5() {
+  var letter05 = letter5.value;
+  if (letter05 != "t") {
+    x5 = document.getElementById("letter5");
+    x5.classList.add("incorrect");
+    }
+  if (letter05 == "t") {
+  y5 = document.getElementById("letter5");
+  y5.classList.remove("incorrect");
+  y5.classList.add("correct");
+  }
+}
+
+function letterGuess6() {
+  var letter06 = letter6.value;
+  if (letter06 != "h") {
+    x6 = document.getElementById("letter6");
+    x6.classList.add("incorrect");
+    }
+  if (letter06 == "h") {
+  y6 = document.getElementById("letter6");
+  y6.classList.remove("incorrect");
+  y6.classList.add("correct");
+  }
+}
+
+function letterGuess7() {
+  var letter07 = letter7.value;
+  if (letter07 != "s") {
+    x7 = document.getElementById("letter7");
+    x7.classList.add("incorrect");
+    }
+  if (letter07 == "s") {
+  y7 = document.getElementById("letter7");
+  y7.classList.remove("incorrect");
+  y7.classList.add("correct");
+  }
+}
+
+function letterGuess8() {
+  var letter08 = letter8.value;
+  if (letter08 != "l") {
+    x8 = document.getElementById("letter8");
+    x8.classList.add("incorrect");
+    }
+  if (letter08 == "l") {
+  y8 = document.getElementById("letter8");
+  y8.classList.remove("incorrect");
+  y8.classList.add("correct");
+  }
+}
+
+function letterGuess9() {
+  var letter09 = letter9.value;
+  if (letter09 != "l") {
+    x9 = document.getElementById("letter9");
+    x9.classList.add("incorrect");
+    }
+  if (letter09 == "l") {
+  y9 = document.getElementById("letter9");
+  y9.classList.remove("incorrect");
+  y9.classList.add("correct");
+  }
+}
+
+function letterGuess10() {
+  var letter010 = letter10.value;
+  if (letter010 != "e") {
+    x10 = document.getElementById("letter10");
+    x10.classList.add("incorrect");
+    }
+  if (letter010 == "e") {
+  y10 = document.getElementById("letter10");
+  y10.classList.remove("incorrect");
+  y10.classList.add("correct");
+  }
+}
+
+function letterGuess11() {
+  var letter011 = letter11.value;
+  if (letter011 != "n") {
+    x11 = document.getElementById("letter11");
+    x11.classList.add("incorrect");
+    }
+  if (letter011 == "n") {
+  y11 = document.getElementById("letter11");
+  y11.classList.remove("incorrect");
+  y11.classList.add("correct");
+  }
+}
+
+function letterGuess12() {
+  var letter012 = letter12.value;
+  if (letter012 != "g") {
+    x12 = document.getElementById("letter12");
+    x12.classList.add("incorrect");
+    }
+  if (letter012 == "g") {
+  y12 = document.getElementById("letter12");
+  y12.classList.remove("incorrect");
+  y12.classList.add("correct");
+  }
+}
+
+function letterGuess13() {
+  var letter013 = letter13.value;
+  if (letter013 != "t") {
+    x13 = document.getElementById("letter13");
+    x13.classList.add("incorrect");
+    }
+  if (letter013 == "t") {
+  y13 = document.getElementById("letter13");
+  y13.classList.remove("incorrect");
+  y13.classList.add("correct");
+  }
+}
+
+function letterGuess14() {
+  var letter014 = letter14.value;
+  if (letter014 != "h") {
+    x14 = document.getElementById("letter14");
+    x14.classList.add("incorrect");
+    }
+  if (letter014 == "h") {
+  y14 = document.getElementById("letter14");
+  y14.classList.remove("incorrect");
+  y14.classList.add("correct");
+  }
+}
+
+function letterGuess15() {
+  var letter015 = letter15.value;
+  if (letter015 != "a") {
+    x15 = document.getElementById("letter15");
+    x15.classList.add("incorrect");
+    }
+  if (letter015 == "a") {
+  y15 = document.getElementById("letter15");
+  y15.classList.remove("incorrect");
+  y15.classList.add("correct");
+  }
+}
+
+function letterGuess16() {
+  var letter016 = letter16.value;
+  if (letter016 != "t") {
+    x16 = document.getElementById("letter16");
+    x16.classList.add("incorrect");
+    }
+  if (letter016 == "t") {
+  y16 = document.getElementById("letter16");
+  y16.classList.remove("incorrect");
+  y16.classList.add("correct");
+  }
+}
+
+function letterGuess17() {
+  var letter017 = letter17.value;
+  if (letter017 != "n") {
+    x17 = document.getElementById("letter17");
+    x17.classList.add("incorrect");
+    }
+  if (letter017 == "n") {
+  y17 = document.getElementById("letter17");
+  y17.classList.remove("incorrect");
+  y17.classList.add("correct");
+  }
+}
+
+function letterGuess18() {
+  var letter018 = letter18.value;
+  if (letter018 != "t") {
+    x18 = document.getElementById("letter18");
+    x18.classList.add("incorrect");
+    }
+  if (letter018 == "t") {
+  y18 = document.getElementById("letter18");
+  y18.classList.remove("incorrect");
+  y18.classList.add("correct");
+  }
+}
+
+function letterGuess19() {
+  var letter019 = letter19.value;
+  if (letter019 != "p") {
+    x19 = document.getElementById("letter19");
+    x19.classList.add("incorrect");
+    }
+  if (letter019 == "p") {
+  y19 = document.getElementById("letter19");
+  y19.classList.remove("incorrect");
+  y19.classList.add("correct");
+  }
+}
+
+function letterGuess20() {
+  var letter020 = letter20.value;
+  if (letter020 != "f") {
+    x20 = document.getElementById("letter20");
+    x20.classList.add("incorrect");
+    }
+  if (letter020 == "f") {
+  y20 = document.getElementById("letter20");
+  y20.classList.remove("incorrect");
+  y20.classList.add("correct");
+  }
+}
+
+function letterGuess21() {
+  var letter021 = letter21.value;
+  if (letter021 != "o") {
+    x21 = document.getElementById("letter21");
+    x21.classList.add("incorrect");
+    }
+  if (letter021 == "o") {
+  y21 = document.getElementById("letter21");
+  y21.classList.remove("incorrect");
+  y21.classList.add("correct");
+  }
+}
+
+function letterGuess22() {
+  var letter022 = letter22.value;
+  if (letter022 != "r") {
+    x22 = document.getElementById("letter22");
+    x22.classList.add("incorrect");
+    }
+  if (letter022 == "r") {
+  y22 = document.getElementById("letter22");
+  y22.classList.remove("incorrect");
+  y22.classList.add("correct");
+  }
+}
+
+function letterGuess23() {
+  var letter023 = letter23.value;
+  if (letter023 != "e") {
+    x23 = document.getElementById("letter23");
+    x23.classList.add("incorrect");
+    }
+  if (letter023 == "e") {
+  y23 = document.getElementById("letter23");
+  y23.classList.remove("incorrect");
+  y23.classList.add("correct");
+  }
+}
+
+function letterGuess24() {
+  var letter024 = letter24.value;
+  if (letter024 != "q") {
+    x24 = document.getElementById("letter24");
+    x24.classList.add("incorrect");
+    }
+  if (letter024 == "q") {
+  y24 = document.getElementById("letter24");
+  y24.classList.remove("incorrect");
+  y24.classList.add("correct");
+  }
+}
+
+function letterGuess25() {
+  var letter025 = letter25.value;
+  if (letter025 != "u") {
+    x25 = document.getElementById("letter25");
+    x25.classList.add("incorrect");
+    }
+  if (letter025 == "u") {
+  y25 = document.getElementById("letter25");
+  y25.classList.remove("incorrect");
+  y25.classList.add("correct");
+  }
+}
+
+function letterGuess26() {
+  var letter026 = letter26.value;
+  if (letter026 != "e") {
+    x26 = document.getElementById("letter26");
+    x26.classList.add("incorrect");
+    }
+  if (letter026 == "e") {
+  y26 = document.getElementById("letter26");
+  y26.classList.remove("incorrect");
+  y26.classList.add("correct");
+  }
+}
+
+function letterGuess27() {
+  var letter027 = letter27.value;
+  if (letter027 != "j") {
+    x27 = document.getElementById("letter27");
+    x27.classList.add("incorrect");
+    }
+  if (letter027 == "j") {
+  y27 = document.getElementById("letter27");
+  y27.classList.remove("incorrect");
+  y27.classList.add("correct");
+  }
+}
+
+function letterGuess28() {
+  var letter028 = letter28.value;
+  if (letter028 != "s") {
+    x28 = document.getElementById("letter28");
+    x28.classList.add("incorrect");
+    }
+  if (letter028 == "s") {
+  y28 = document.getElementById("letter28");
+  y28.classList.remove("incorrect");
+  y28.classList.add("correct");
+  }
+}
+
+function letterGuess29() {
+  var letter029 = letter29.value;
+  if (letter029 != "o") {
+    x29 = document.getElementById("letter29");
+    x29.classList.add("incorrect");
+    }
+  if (letter029 == "o") {
+  y29 = document.getElementById("letter29");
+  y29.classList.remove("incorrect");
+  y29.classList.add("correct");
+  }
+}
+
+function letterGuess30() {
+  var letter030 = letter30.value;
+  if (letter030 != "n") {
+    x30 = document.getElementById("letter30");
+    x30.classList.add("incorrect");
+    }
+  if (letter030 == "n") {
+  y30 = document.getElementById("letter30");
+  y30.classList.remove("incorrect");
+  y30.classList.add("correct");
+  }
+}
+
+function letterGuess31() {
+  var letter031 = letter31.value;
+  if (letter031 != "t") {
+    x31 = document.getElementById("letter31");
+    x31.classList.add("incorrect");
+    }
+  if (letter031 == "t") {
+  y31 = document.getElementById("letter31");
+  y31.classList.remove("incorrect");
+  y31.classList.add("correct");
+  }
+}
+function ToggleButton(){  
+const buttonWhite = document.querySelectorAll(".buttonWhite");
+  // console.log(buttonWhite);
+buttonWhite.forEach(buttonWhite => {
+  switch(buttonWhite.id){
+    case"letter1":
+      buttonWhite.value="x"
+      break;
+    case"letter2":
+      buttonWhite.value="i"
+      break;
+    case "letter3":
+      buttonWhite.value="m"
+      break;
+    case"letter4":
+    buttonWhite.value="a"
+      break;
+    case "letter5":
+      buttonWhite.value="t"
+      break;   
+    case"letter6":
+    buttonWhite.value="h"
+      break;
+    case "letter7":
+      buttonWhite.value="s"
+      break;
+    case "letter8":
+      buttonWhite.value="l"
+      break;
+    case"letter9":
+    buttonWhite.value="l"
+      break;
+    case "letter10":
+      buttonWhite.value="e"
+      break;  
+    case"letter11":
+    buttonWhite.value="n"
+      break;
+    case "letter12":
+      buttonWhite.value="g"
+      break;
+    case "letter13":
+      buttonWhite.value="t"
+      break;
+    case"letter14":
+    buttonWhite.value="h"
+      break;
+    case "letter15":
+      buttonWhite.value="a"
+      break;  
+    case"letter16":
+    buttonWhite.value="t"
+      break;
+    case "letter17":
+      buttonWhite.value="n"
+      break;
+    case "letter18":
+      buttonWhite.value="t"
+      break;
+    case"letter19":
+    buttonWhite.value="p"
+      break;
+    case "letter20":
+      buttonWhite.value="f"
+      break;  
+    case"letter21":
+    buttonWhite.value="o"
+      break;
+    case "letter22":
+      buttonWhite.value="r"
+      break;
+    case "letter23":
+      buttonWhite.value="e"
+      break;
+    case"letter24":
+    buttonWhite.value="k"
+      break;
+    case "letter25":
+      buttonWhite.value="u"
+      break;  
+    case"letter26":
+    buttonWhite.value="e"
+      break;
+    case "letter27":
+      buttonWhite.value="j"
+      break;
+    case "letter28":
+      buttonWhite.value="s"
+      break;
+    case"letter29":
+    buttonWhite.value="o"
+      break;
+    case "letter30":
+      buttonWhite.value="n"
+      break;             
+    case"letter31":
+    buttonWhite.value="t"
+      break;
+  }
+});
+}
+  
+  
+
+
